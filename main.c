c6bfc7a847ea59acb1bc44b6e9ef3e9057442862
Maccraft123 <maccraft123mc@gmail.com>
2021-10-21 14:02:09 +0000

    it works now

--- main.c
+++ main.c
@@ -5,8 +5,14 @@
 #include <unistd.h>
 #include <string.h>
 
-#define JSPATH "" // TODO
+#define ANALPATH "/dev/input/by-path/platform-analog-event-joystick"
 #define KEYPATH "/dev/input/by-path/platform-gpio-keys-event-joystick"
+
+int outfd;
+int keyfd;
+int analfd;
+
+pthread_t handleAnal;
 
 // yoinked directly from kernel documentation
 void emit(int fd, int type, int code, int val)
@@ -24,11 +30,47 @@ void emit(int fd, int type, int code, int val)
 	write(fd, &ie, sizeof(ie));
 }
 
+static void setup_abs(int fd, int fd2, int unsigned chan)
+{
+	if (ioctl(fd, UI_SET_ABSBIT, chan))
+		perror("UI_SET_ABSBIT");
+
+	struct uinput_abs_setup s =
+	{
+		.code = chan,
+		.absinfo = { .minimum = 0,  .maximum = 0 },
+	};
+	
+	ioctl(fd2, EVIOCGABS(chan), &s.absinfo);
+
+	if (ioctl(fd, UI_ABS_SETUP, &s))
+		perror("UI_ABS_SETUP");
+}
+
+void *analHandler(void *unused)
+{
+
+	int rd, i;
+	struct input_event ev[4];
+
+	while(1)
+	{
+		rd = read(analfd, ev, sizeof(struct input_event) * 4);
+		if (rd > 0)
+		{
+			for (i = 0; i < rd / sizeof(struct input_event) * 4; i++)
+			{
+				emit(outfd, ev[i].type, ev[i].code, ev[i].value);
+			}
+		}
+	}
+}
+
 int main(void)
 {
-	int keyfd = open(KEYPATH, O_RDONLY);
-	//int jsfd = open(JSPATH, O_RDONLY);
-	int outfd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+	outfd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+	keyfd = open(KEYPATH, O_RDONLY);
+	analfd = open(ANALPATH, O_RDONLY);
 
 	struct uinput_setup usetup;
 
@@ -53,6 +95,12 @@ int main(void)
 	ioctl(outfd, UI_SET_KEYBIT, BTN_SELECT);
 	ioctl(outfd, UI_SET_KEYBIT, BTN_START);
 
+	ioctl(outfd, UI_SET_EVBIT, EV_ABS);
+	setup_abs(outfd, analfd, ABS_X);
+	setup_abs(outfd, analfd, ABS_Y);
+	setup_abs(outfd, analfd, ABS_RX);
+	setup_abs(outfd, analfd, ABS_RY);
+
 	memset(&usetup, 0, sizeof(usetup));
 	usetup.id.bustype = BUS_USB;
 	usetup.id.vendor = 0x1234;
@@ -61,15 +109,16 @@ int main(void)
 
 	ioctl(outfd, UI_DEV_SETUP, &usetup);
 	ioctl(outfd, UI_DEV_CREATE);
-
-	// make sure nothing can read our inputs
+	
 	ioctl(keyfd, EVIOCGRAB, 1);
-	//ioctl(jsfd, EVIOCGRAB, 1);
+	ioctl(analfd, EVIOCGRAB, 1);
+
+	pthread_create(&handleAnal, NULL, analHandler, NULL);
 
 	int rd, i;
 	struct input_event ev[4];
 
-	while (1)
+	while(1)
 	{
 		rd = read(keyfd, ev, sizeof(struct input_event) * 4);
 		if (rd > 0)
@@ -80,6 +129,4 @@ int main(void)
 			}
 		}
 	}
-
-	return 0;
 }
